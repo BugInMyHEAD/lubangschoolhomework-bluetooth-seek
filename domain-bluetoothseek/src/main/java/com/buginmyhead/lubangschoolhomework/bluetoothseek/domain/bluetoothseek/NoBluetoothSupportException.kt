a07b5b3836bcfe2eeefb0558a52d823531c27d3b
BugInMyHEAD <buginmyhead@gmail.com>
2022-11-21 01:23:12 +0900

    Add bluetooth exceptions

---
+++ domain-bluetoothseek/src/main/java/com/buginmyhead/lubangschoolhomework/bluetoothseek/domain/bluetoothseek/NoBluetoothSupportException.kt
@@ -0,0 +1,3 @@
+package com.buginmyhead.lubangschoolhomework.bluetoothseek.domain.bluetoothseek
+
+open class NoBluetoothSupportException : RuntimeException()
---
+++ domain-bluetoothseek/src/main/java/com/buginmyhead/lubangschoolhomework/bluetoothseek/domain/bluetoothseek/NoPairedDeviceException.kt
@@ -0,0 +1,3 @@
+package com.buginmyhead.lubangschoolhomework.bluetoothseek.domain.bluetoothseek
+
+open class NoPairedDeviceException(cause: Throwable? = null) : RuntimeException(cause)
