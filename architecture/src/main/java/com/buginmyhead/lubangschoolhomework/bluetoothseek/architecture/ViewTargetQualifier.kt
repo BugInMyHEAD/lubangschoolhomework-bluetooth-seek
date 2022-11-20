efeeca80beee6436ef3e544c74a1a20561ecff45
BugInMyHEAD <buginmyhead@gmail.com>
2022-11-21 01:23:10 +0900

    Add ViewTargetQualifier

---
+++ architecture/src/main/java/com/buginmyhead/lubangschoolhomework/bluetoothseek/architecture/ViewTargetQualifier.kt
@@ -0,0 +1,7 @@
+package com.buginmyhead.lubangschoolhomework.bluetoothseek.architecture
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.SOURCE)
+annotation class ViewTargetQualifier
