e2421def47e7130a2017bd0b5b93f4c48cc6e705
BugInMyHEAD <buginmyhead@gmail.com>
2022-11-21 01:23:11 +0900

    Rename ReadOnlyRepository to ValueOnlyRepository

--- architecture/src/main/java/com/buginmyhead/lubangschoolhomework/bluetoothseek/architecture/ValueOnlyRepository.kt
+++ architecture/src/main/java/com/buginmyhead/lubangschoolhomework/bluetoothseek/architecture/ValueOnlyRepository.kt
@@ -2,7 +2,7 @@ package com.buginmyhead.lubangschoolhomework.bluetoothseek.architecture
 
 import io.reactivex.rxjava3.core.Single
 
-interface ReadOnlyRepository<T : Any> {
+interface ValueOnlyRepository<T : Any> {
 
     val value: Single<T>
 
